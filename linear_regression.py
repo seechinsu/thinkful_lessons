8093b40c1e2a0b8b24c194a52f3cd4b63044c3c0
seech <seechinsu@gmail.com>
2017-03-02 01:49:13 -0500

    lesson 2.3.2

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
 
@@ -10,12 +12,31 @@ loansData['FICO.Score'] = map(lambda s: int(s[0]),map(lambda s: str(s).split('-'
 #print loansData['Interest.Rate'][0:5]
 #print loansData['Loan.Length'][0:5]
 #print loansData['FICO.Score'][0:5]
-
-print loansData[0:5]
+#print loansData[0:5]
 
 #plt.figure()
 #p = loansData['FICO.Score'].hist()
 #plt.show()
 
-a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
+#a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
+#plt.show()
+
+intrate = loansData['Interest.Rate']
+loanamt = loansData['Amount.Requested']
+fico = loansData['FICO.Score']
+
+y = np.matrix(intrate).transpose()
+x1 = np.matrix(fico).transpose()
+x2 = np.matrix(loanamt).transpose()
+
+x = np.column_stack([x1, x2])
+X = sm.add_constant(x)
+model = sm.OLS(y, X)
+
+f = model.fit()
+print f.params
+print f.pvalues
+print f.summary()
+
+plt.scatter(x1, y)
 plt.show()
